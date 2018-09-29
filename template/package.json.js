e1728617d8bd9df9fe2ed5ee1807a72264800343
imcuttle <moyuyc95@gmail.com>
2018-09-29 16:47:07 +0800

    HUSKY_GIT_PARAMS

--- template/package.json.js
+++ template/package.json.js
@@ -82,7 +82,7 @@ module.exports = function({ _, test, lerna, changelog, documentation, descriptio
 
   if (changelog) {
     appendCmd('scripts.version', 'npm run changelog')
-    pkg.husky.hooks.commitmsg = 'commitlint -e $GIT_PARAMS'
+    pkg.husky.hooks.commitmsg = 'commitlint -e $HUSKY_GIT_PARAMS'
     pkg.scripts.changelog = 'conventional-changelog -p angular -i CHANGELOG.md -s -r 0 && git add CHANGELOG.md'
     pkg.commitlint = {
       extends: ['@commitlint/config-conventional']
