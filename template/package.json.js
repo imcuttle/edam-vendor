fed4d8a2ca212f2911f6d5655fbb0eb9373bd2ff
imcuttle <moyuyc95@gmail.com>
2018-10-10 21:17:43 +0800

    update keyword

--- template/package.json.js
+++ template/package.json.js
@@ -21,7 +21,7 @@ module.exports = function({ _, test, lerna, changelog, documentation, descriptio
       node: '>=6'
     },
     files: ['lib', 'src'],
-    keywords: [_.git.name, name],
+    keywords: [_.git.name].concat(name.split('-')).concat(name),
     typings: language === 'typescript' ? 'lib/index.d.ts' : 'index.d.ts',
     license: 'MIT',
     repository: _.git.name + '/' + name
