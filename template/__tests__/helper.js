e9867f0cd3fbcbbf126d2248107a681f8710597a
imcuttle <moyuyc95@gmail.com>
2018-10-28 00:01:35 +0800

    update fixture in test helper

--- template/__tests__/helper.js
+++ template/__tests__/helper.js
@@ -5,7 +5,7 @@
 const nps = require('path')
 
 function fixture() {
-  return nps.join.apply(nps, [__dirname, 'fixture'].concat(arguments))
+  return nps.join.apply(nps, [__dirname, 'fixture'].concat([].slice.call(arguments)))
 }
 
 module.exports = {
