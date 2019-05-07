2b3cfd760a7d33d75e638f7ad5ee1a6862be7bfd
imcuttle <moyuyc95@gmail.com>
2019-05-07 20:00:07 +0800

    use pascalCase

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -6,7 +6,7 @@ export default [
     input: 'index.js',
     output: [
       {
-        name: 'OpenOneWindow',
+        name: '{{{pascalCase name}}}',
         file: pkg.browser,
         format: 'umd'
       },
